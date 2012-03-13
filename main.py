f0e89cbda447fcfa778f9e61f4182af4a37cf117
jeffkaplan@caltech.edu <jeffkaplan@caltech.edu>
2012-03-12 23:01:19 -0700

    Playing around.  Sort of got working printing cursor location to screen.

--- main.py
+++ main.py
@@ -14,10 +14,25 @@ label = pyglet.text.Label('Hello, Jewel Theif',
                           font_size=36,
                           x=window.width//2, y=window.height//2,
                           anchor_x='center', anchor_y='center')
+positionLabel = pyglet.text.Label()
+
+@window.event
+def on_mouse_motion(x, y, dx, dy):
+    window.clear()
+    positionLabel = pyglet.text.Label('x = ' + str(x) + '  y = ' + str(y),
+                          font_name='Times New Roman',
+                          font_size=24,
+                          x=window.width//4, y=window.height//4,
+                          anchor_x='center', anchor_y='center')
+    positionLabel.draw()
+
 @window.event
 def on_draw():
-    window.clear()
-    label.draw()
+    #window.clear()
+    #positionLabel.draw()
+    #label.draw()
+    pass
 
-
+#Prints to stdout every event that is handled
+window.push_handlers(pyglet.window.event.WindowEventLogger())
 pyglet.app.run()
